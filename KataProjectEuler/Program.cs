a220286cd2680014a4cc81140ae787cf0d485e7b
yu.dyachenko <felmian123456@gmail.com>
2020-02-11 13:26:13 +0200

    save work kata012

--- KataProjectEuler/Program.cs
+++ KataProjectEuler/Program.cs
@@ -9,49 +9,54 @@ namespace KataProjectEuler
     {
         static void Main()
         {
-            //001
-            IProblemResult _001 = new _001_MultiplesOf3and5();
-            _001.GetResult();
+            ////001
+            //IProblemResult _001 = new _001_MultiplesOf3and5();
+            //_001.GetResult();
 
-            //002
-            IProblemResult _002 = new _002_EvenFibonaccinumbers();
-            _002.GetResult();
+            ////002
+            //IProblemResult _002 = new _002_EvenFibonaccinumbers();
+            //_002.GetResult();
 
-            //003
-            IProblemResult _003 = new _003__LargestPprimeFactor();
-            //_003.GetResult(); //Heavy CPU load
+            ////003
+            //IProblemResult _003 = new _003__LargestPprimeFactor();
+            ////_003.GetResult(); //Heavy CPU load
 
-            //004
-            IProblemResult _004 = new _004_LargestPalindromeProduct();
-            _004.GetResult();
+            ////004
+            //IProblemResult _004 = new _004_LargestPalindromeProduct();
+            //_004.GetResult();
 
-            //005
-            IProblemResult _005 = new _005_SmallestMultiple();
-            _005.GetResult();
+            ////005
+            //IProblemResult _005 = new _005_SmallestMultiple();
+            //_005.GetResult();
 
-            //006
-            IProblemResult _006 = new _006_SumSquareDifference();
-            _006.GetResult();
+            ////006
+            //IProblemResult _006 = new _006_SumSquareDifference();
+            //_006.GetResult();
 
-            //007
-            IProblemResult _007 = new _007_10001stPrime();
-            _007.GetResult();
+            ////007
+            //IProblemResult _007 = new _007_10001stPrime();
+            //_007.GetResult();
 
-            //008
-            IProblemResult _008 = new _008__LargestProductInAseries();
-            _008.GetResult();
+            ////008
+            //IProblemResult _008 = new _008__LargestProductInAseries();
+            //_008.GetResult();
 
-            //009
-            IProblemResult _009 = new _009_SpecialPythagoreanTriplet();
-            _009.GetResult();
+            ////009
+            //IProblemResult _009 = new _009_SpecialPythagoreanTriplet();
+            //_009.GetResult();
 
-            //010
-            IProblemResult _010 = new _010_SummationOfPrimes();
-            _010.GetResult();
+            ////010
+            //IProblemResult _010 = new _010_SummationOfPrimes();
+            //_010.GetResult();
 
-            //011
-            IProblemResult _011= new _011_LargestProductInAgrid();
-            _011.GetResult();
+            ////011
+            //IProblemResult _011= new _011_LargestProductInAgrid();
+            //_011.GetResult();
+
+            //012
+            IProblemResult _012 = new _012_HighlyDivisibleTriangularNumber();
+            _012.GetResult();
+
 
 
 
